1d49361d136fd54d37aef1fdf2a017209386bc74
Potassium <brainiacgamer444@gmail.com>
2023-09-02 17:47:41 +0545

    Wrap contents into another div for styling

--- src/home.js
+++ src/home.js
@@ -2,12 +2,24 @@ function createHomePage() {
     const home = document.createElement("div");
     home.classList.add("home");
 
+    // First part
+    const titleImage = document.createElement("div");
+    titleImage.classList.add("title-image");
+
     const title = document.createElement("h3");
     title.textContent = "Welcome to Los Santos Restaurant";
+    title.classList.add("home-title");
 
     const introImage = document.createElement("img");
     introImage.src = "images/restaurant-pic.jpg";
     introImage.alt = "Restaurant pic with swimming pool and drinks view";
+
+    titleImage.appendChild(title);
+    titleImage.appendChild(introImage);
+
+    // Second part
+    const introPara = document.createElement("div");
+    introPara.classList.add("intro-para");
 
     const intro = document.createElement("p");
     intro.textContent = "The best restaurant in the city. Any doubts? No? Okay. That's what I thought.";
@@ -15,10 +27,11 @@ function createHomePage() {
     const secondIntro = document.createElement("p");
     secondIntro.textContent = "Our focus is to offer high quality international cuisine with the elegance of fine-dining while in a relaxed and comfortable atmosphere. Our menu offers freshly made and unique innovative dishes and our service is attentive and gracious.";
 
-    home.appendChild(title);
-    home.appendChild(introImage);
-    home.appendChild(intro);
-    home.appendChild(secondIntro);
+    introPara.appendChild(intro);
+    introPara.appendChild(secondIntro);
+
+    home.appendChild(titleImage);
+    home.appendChild(introPara);
 
     return home;
 }
