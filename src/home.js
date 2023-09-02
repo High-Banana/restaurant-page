d324ef08d59a6523bd8a64a09389ed1df5b36bd5
Potassium <brainiacgamer444@gmail.com>
2023-09-02 16:49:59 +0545

    Make images folder and create paragraph texts and image for home page

--- src/home.js
+++ src/home.js
@@ -4,15 +4,22 @@ function createHomePage() {
     const title = document.createElement("h3");
     title.textContent = "Welcome to Los Santos Restaurant";
 
-    home.appendChild(title);
-    
-    return home;
-}
+    const introImage = document.createElement("img");
+    introImage.src = "images/restaurant-pic.jpg";
+    introImage.alt = "Restaurant pic with swimming pool and drinks view";
 
-function createParagraph(text) {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = text;
-    return paragraph;
+    const intro = document.createElement("p");
+    intro.textContent = "The best restaurant in the city. Any doubts? No? Okay. That's what I thought.";
+
+    const secondIntro = document.createElement("p");
+    secondIntro.textContent = "Our focus is to offer high quality international cuisine with the elegance of fine-dining while in a relaxed and comfortable atmosphere. Our menu offers freshly made and unique innovative dishes and our service is attentive and gracious.";
+
+    home.appendChild(title);
+    home.appendChild(introImage);
+    home.appendChild(intro);
+    home.appendChild(secondIntro);
+
+    return home;
 }
 
 const loadHomePage = () => {
